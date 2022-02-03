6e2665243bff76e7ad98e4a0e6bfc65cf2e61894
emmanuel <emmanuel@example.com>
2022-02-03 16:52:50 -0300

    modif.

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -158,10 +158,10 @@ function operadoresLogicos(num1, num2, num3) {
     return 'Numero 1 es mayor y positivo';
   }else if(num1 < 0 || num2 < 0 || num3 < 0){
     return 'Hay negativos';
-  }if(num3 > num1 && num3 > num2){
+  }else if(num3 > num1 && num3 > num2){
     num3 = num3 +1;
     return num3;
-  }if (num1 === 0 || num2 === 0 || num3 === 0){
+  }else if (num1 === 0 || num2 === 0 || num3 === 0){
     return 'Error';
   }else {
     return false;
