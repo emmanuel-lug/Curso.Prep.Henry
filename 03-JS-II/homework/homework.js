3537d891a671f0256868dc34f54938030d7dc1c3
emmanuel <emmanuel@example.com>
2022-02-03 16:30:54 -0300

    modif.

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -152,10 +152,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 > num2 && num1 > num3 && num1 > 0){
+    return 'Numero 1 es mayor y positivo';
+  }else if(num1 < 0 || num2 < 0 || num3 < 0){
+    return 'Hay negativos';
+  }if(num3 > num1 && num3 > num2){
+    num3 = num3 +1;
+    return num3;
+  }if (num1 === 0 || num2 === 0 || num3 === 0){
+    return 'Error';
+  }else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
-  // Devuelve "true" si "numero" es primo
+  //11 Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
@@ -163,27 +175,27 @@ function esPrimo(numero) {
 }
 
 function esVerdadero(valor){
-  //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+  //12 Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
 }
 
 function tablaDelSeis(){
-  //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+  //13 Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
   
 }
 
 function tieneTresDigitos(numero){
-  //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+  //14 Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
   
 }
 
 function doWhile(numero) {
-  //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+  //15 Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
 }
