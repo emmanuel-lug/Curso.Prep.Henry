79d96805e57ca2c3cdedaaddd5dfd222b018cbfc
emmanuel <emmanuel@example.com>
2022-02-04 20:30:33 -0300

    modif.

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -188,12 +188,12 @@ function empiezaConNueve(n) {
   //15 Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  if(n.charAt[0] === 9)
-  {
-    return true;
-  }else {
-    return false;
-  }
+ var div = 1;
+ while( (n / div) > -10 && (n / div) < 10 && (n / div) !== 0)
+ {
+   div  =  div * 10
+    }
+    return (n/div);
 }
 
 
