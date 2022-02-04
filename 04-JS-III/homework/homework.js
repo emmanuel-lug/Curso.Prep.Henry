d8a264cadc33e7aff4b3499dae298d3ef0493d21
emmanuel <emmanuel@example.com>
2022-02-04 19:59:14 -0300

    modif.

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -203,12 +203,12 @@ function todosIguales(arreglo) {
   //16 Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí
-  for(var i = 0; i< arreglo.lenght - 1; i++){
+  for(var i =0; i < arreglo.lenght - 1; i++){
     if(arreglo[i] !== arreglo[i+1]){
-    return false;  
+    return false  
     }
   }
-  return true;
+  return true
 } 
 
 
