eee61575b5012395ffcc53b39ae0b2292b01bbc9
emmanuel <emmanuel@example.com>
2022-02-06 21:35:31 -0300

    modif.

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -46,7 +46,7 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
-  delete objeto.unaPropiedad;
+  delete objeto[unaPropiedad];
   return objeto;
 }
 
