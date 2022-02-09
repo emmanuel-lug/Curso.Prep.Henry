8cc91442d3abbbef57fddb6fdde823d54cb1d3b1
emmanuel <emmanuel@example.com>
2022-02-08 23:39:28 -0300

    modif.

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -61,12 +61,13 @@ function agregarStringInvertida() {
     this.nombre = nombre,
     this.apellido = apellido,
     this.edad = edad,
-    this.domicilio = domicilio,
+    this.domicilio = domicilio
     this.detalle = function(){
       return {nombre: this.nombre ,
               apellido: this.apellido,
               edad: this.edad,
-              domicilio: this.domicilio}
+              domicilio: this.domicilio
+            }
     }
     }
     
