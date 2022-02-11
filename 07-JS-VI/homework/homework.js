2c78a6f46c81b3bc2a44ca9597e4197b876b1d53
emmanuel <emmanuel@example.com>
2022-02-11 14:13:42 -0300

    modif.

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,8 +4,8 @@ function mayuscula(nombre) {
   //1 La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return nombre[0] = nombre[0].toUpperCase();
-  
+  nombre[0] = nombre[0].toUpperCase();
+  return nombre; 
 }
 
 function invocarCallback(cb) {
